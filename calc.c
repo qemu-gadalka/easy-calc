da7aa202013f8d4e47ccd6b929322f6e242d40e9
qemu <requests567@gmail.com>
2025-09-16 10:48:36 +0300

    Update calc.c

--- calc.c
+++ calc.c
@@ -2,38 +2,34 @@
 #include <stdlib.h>
 
 int main() {
-    SetCurrentDirectoryA("C:\\");
-    int a;
-    printf("");
-    scanf("%d", &a);
-    char b;
-    printf("+, -, *, / >>> ");
-    scanf(" %c", &b);
-    int c;
-    printf("");
-    scanf("%d", &c);
-    printf("%d %c %d\n", a, b, c);
+    int a, c;
+    char op;
+
+    printf(" » ");
+    if (scanf("%d %c %d", &a, &op, &c) != 3) {
+        printf("! Invalid input\n");
+        return 1;
+    }
+
     int result;
-    if (b == '+') {
+    if (op == '+') {
         result = a + c;
-    } else if (b == '-') {
+    } else if (op == '-') {
         result = a - c;
-    } else if (b == '*') {
+    } else if (op == '*') {
         result = a * c;
-    } else if (b == '/') {
+    } else if (op == '/') {
         if (c != 0) {
             result = a / c;
         } else {
-            printf("Error: Division by zero\n");
-            system("pause");
+            printf("! Division by zero\n");
             return 1;
         }
     } else {
-        printf("Error: Unknown operation\n");
-        system("pause");
+        printf("! Unknown Operation\n");
         return 1;
     }
-    printf("Result: %d\n", result);
-    system("pause");
+
+    printf("! Result → %d\n", result);
     return 0;
 }
